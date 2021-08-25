b0774347ac6378466fac5e50b3270c497476bc38
ali noorbala <a.noorbala@sain.ir>
2021-08-25 11:02:38 +0430

    Writing the Guestbook Admin App - Step 2 of 6

--- modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookWebPortletKeys.java
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookWebPortletKeys.java
@@ -8,4 +8,7 @@ public class GuestbookWebPortletKeys {
 	public static final String GUESTBOOKWEB =
 		"com_liferay_docs_guestbook_portlet_GuestbookWebPortlet";
 
+	public static final String GUESTBOOK_ADMIN =
+			"com_liferay_docs_guestbook_portlet_GuestbookAdminPortlet";
+
 }
